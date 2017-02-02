5f8541956b2ae92b93dcc816b10007b975f86501
Peter Williams <peter@newton.cx>
2017-02-02 09:40:24 -0500

    Modernize the Windows setup.
    This updates the Windows stuff based on the experience I've gained building
    the downstream X.org libraries this far. The key new bit is that it looks like
    we're really supposed to use $LIBRARY_PREFIX as our install prefix rather than
    $PREFIX. I also run autoreconf which looks like it will be necessary for the
    Windows builds. Not actually needed for this package since it just installs
    macro files, but I want the build scripts to be as consistent as possible.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,27 +3,51 @@
 set -e
 IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
 
-# Some X.org packages have config.{guess,sub} too old to properly build on
-# 64-bit Windows! Because this package stores generic X.org build helpers, we
-# use it to distribute more recent versions of those file. Other X.org
-# packages should have this one as a build-time dependency and copy the files
-# into their unpacked source directories.
+# Adopt a Unix-friendly path if we're on Windows (see bld.bat).
+[ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
 
-mkdir -p $PREFIX/share/util-macros
+# On Windows we want $LIBRARY_PREFIX not $PREFIX.
+if [ -n "$LIBRARY_PREFIX" ] ; then
+    useprefix="$LIBRARY_PREFIX"
+else
+    useprefix="$PREFIX"
+fi
+
+# On Windows we need to regenerate the configure scripts.
+if [ -n "$VS_MAJOR" ] ; then
+    am_version=1.15 # keep sync'ed with meta.yaml
+    export ACLOCAL=aclocal-$am_version
+    export AUTOMAKE=automake-$am_version
+    autoreconf_args=(
+        --force
+        --install
+        -I "$PREFIX/Library/mingw-w$ARCH/share/aclocal"
+    )
+    autoreconf "${autoreconf_args[@]}"
+fi
+
+# We used to provide our own config.{guess,sub} on Windows; now we are
+# transitioning to running autoreconf in all Windows builds, since the
+# distributed configure scripts have a lot of Windows special-casing that
+# fails on msys2 unless we use msys2's autotools. But to smooth the transition
+# we'll keep distributing the files for a bit.
+
+mkdir -p $useprefix/share/util-macros
 
 for f in config.guess config.sub ; do
     cp -p $RECIPE_DIR/$f .
-    cp -p $RECIPE_DIR/$f $PREFIX/share/util-macros/
+    cp -p $RECIPE_DIR/$f $useprefix/share/util-macros/
 done
 
-export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
+export PKG_CONFIG_LIBDIR=$useprefix/lib/pkgconfig:$useprefix/share/pkgconfig
 
 configure_args=(
-    --prefix=$PREFIX
+    --prefix=$useprefix
     --disable-dependency-tracking
     --disable-selective-werror
     --disable-silent-rules
 )
+
 ./configure "${configure_args[@]}"
 make -j${CPU_COUNT}
 make install
